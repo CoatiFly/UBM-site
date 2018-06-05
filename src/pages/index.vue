fa75f829681da0f476a68533a9b82ee7ce307e6e
xiep 解澎 <xiep@i.bekylin.com>
2018-06-05 18:42:39 +0800

    fix style

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,7 +11,8 @@
       </div>
       <div class="list_block">
         <ul class="shell">
-          <li class="item title_list visit center" v-on:click="goPage('travel')"><p>{{$t("index.visit")}}</p></li>
+          <li class="item title_list visit center" v-if="!isPC && language == 'en'" v-on:click="goPage('travel')"><p>WHY</br>VISIT</p></li>
+          <li class="item title_list visit center" v-else v-on:click="goPage('travel')"><p>{{$t("index.visit")}}</p></li>
           <li class="item title_list exhibit center" v-on:click="goPage('travel')"><p>{{$t("index.exhibit")}}</p></li>
           <li class="item title_list fsa center" v-on:click="goPage('sessions')"><p>{{$t("index.fsa")}}</p></li>
           <li class="item red_bg mobile_full">
